d94fe3e261de5b253687a4f2f250211355af873d
Marijn van Vliet <w.m.vanvliet@gmail.com>
2026-01-15 22:57:04 +0200

    remove files we will code in the live demo

--- dodo.py
+++ dodo.py
@@ -23,33 +23,3 @@ def task_preprocessing():
             targets=[fname.filtered(subject=subject)],
             actions=[f"python 02_preprocessing.py {subject}"],
         )
-
-
-def task_epochs():
-    """Cut epochs and compute evokeds."""
-    for subject in subjects:
-        yield dict(
-            name=f"sub-{subject:02d}",
-            file_dep=[fname.filtered(subject=subject), "03_epochs.py"],
-            targets=[fname.epochs(subject=subject), fname.evokeds(subject=subject)],
-            actions=[f"python 03_epochs.py {subject}"],
-        )
-
-
-def task_ga():
-    """Compute grand average evoked potentials."""
-    return dict(
-        file_dep=[fname.evokeds(subject=subject) for subject in subjects]
-        + ["04_grand_average.py"],
-        targets=[fname.ga_evokeds],
-        actions=["python 04_grand_average.py"],
-    )
-
-
-def task_figure1():
-    """Make figure 1 of the manuscript: grand average evokeds"""
-    return dict(
-        file_dep=[fname.ga_evokeds, "figure_ga_evokeds.py"],
-        targets=[fname.figure_ga_evokeds],
-        actions=["python figure_ga_evokeds.py"],
-    )
